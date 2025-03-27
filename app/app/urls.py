35b16fe59221c360ce6b4c79d78618fee4eee890
Pamela Redjepovska <pamela.redjepovska@hornetsecurity.com>
2025-03-27 12:07:14 +0100

    Add additional recipe API functionalities and tests

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema')),
-    path('api/user/', include('user.urls'))
+    path('api/user/', include('user.urls')),
+    path('api/recipe/', include('recipe.urls'))
 ]
